92eb5493f66eb59527fe3fb6cb047d39f4bbe306
Johan Bové <jbove@deloitte.de>
2017-01-18 18:14:48 +0100

    Persisting our State with Firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAvaQrZYqUzg0vGDNWVsGbtxnUDZJlG8Vc",
+  authDomain: "catch-of-the-day-jan-17.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-jan-17.firebaseio.com"
+});
+
+export default base;
