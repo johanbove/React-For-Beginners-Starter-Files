11e80fad8418cc649af198f93bf5a7cca97beb27
Johan Bové <johan.bove@gmail.com>
2017-01-18 08:04:49 +0100

    All About React Router
    Video 12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 
+
+// ES6 class
 class StorePicker extends React.Component {
-  // Better for performance
+  // Better for performance since goToStore will live once.
+  // Do this for methods that you need to use more than once.
   // constructor() {
   //   super();
   //   this.goToStore = this.goToStore.bind(this);
@@ -10,13 +13,16 @@ class StorePicker extends React.Component {
 
   goToStore(event) {
     event.preventDefault();
-    console.log('You changed the URL');
     // first grab the text from the box
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
     // second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
+    // These are anymous functions
+    // So `this` equals the StorePicker component
     // (e) => this.goToStore(e)
     // this.goToStore().bind(this)
     return (
@@ -27,6 +33,10 @@ class StorePicker extends React.Component {
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
